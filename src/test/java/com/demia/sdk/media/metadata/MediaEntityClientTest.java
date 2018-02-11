0e328eb8dadd14e5c78affbd683905c7aa5e239c
Ranjit Aneesh <ranjitaneesh@gmail.com>
2018-02-11 22:27:49 +0530

    Develop (#4)
    * Changed request factory defaults
    
    * Maven setup for building jar

--- src/test/java/com/demia/sdk/media/metadata/MediaEntityClientTest.java
+++ src/test/java/com/demia/sdk/media/metadata/MediaEntityClientTest.java
@@ -10,114 +10,129 @@ import org.junit.Before;
 import org.junit.FixMethodOrder;
 import org.junit.Test;
 import org.junit.runners.MethodSorters;
-import org.springframework.web.client.HttpClientErrorException;
 
 import com.demia.sdk.core.AuthenticationClient;
 
 @FixMethodOrder(MethodSorters.NAME_ASCENDING)
 public class MediaEntityClientTest {
-    
+
     private AuthenticationClient authenticationClient = new AuthenticationClient();
     private MediaEntityClient mediaEntityClient = new MediaEntityClient();
-    
+
     @Before
     public void setup() {
         mediaEntityClient = new MediaEntityClient();
     }
-    
 
-    //@Test
+    // @Test
     public void test1CreateMediaEntity() {
         authenticationClient.authenticate();
-        
+
         MediaEntity entity = new MediaEntity();
         entity.setName("Test 123");
         entity.setPath("l1/l2");
-        entity = mediaEntityClient.createMediaEntity(entity, "C:\\Users\\Ranjit\\Pictures\\sample.jpg");
+        entity = mediaEntityClient.createMediaEntity(entity, "C:\\Users\\Ranjit\\Pictures\\happybday.jpg");
         System.out.println(entity);
-        
+
     }
 
-    //@Test
+    // @Test
     public void test2GetMediaEntity() {
-        //Create
+        // Create
         authenticationClient.authenticate();
         MediaEntity entity = new MediaEntity();
         entity.setName("Test 123");
         entity.setPath("l1/l2");
         entity = mediaEntityClient.createMediaEntity(entity, "C:\\Users\\Ranjit\\Pictures\\sample.jpg");
-        
+
         MediaEntity entityRetrieved = mediaEntityClient.getMediaEntity(entity.getAssetId());
         assertNotNull(entityRetrieved);
-        
+
     }
 
-    //@Test
+    // @Test
     public void test3GetAllMediaEntities() {
         authenticationClient.authenticate();
-        MediaEntities entities = mediaEntityClient.getMediaEntities(0, 10, SortElement.CREATED_DATETIME, Order.DESCENDING);
+        MediaEntities entities = mediaEntityClient.getMediaEntities(0, 10, SortElement.CREATED_DATETIME,
+                Order.DESCENDING);
         assertNotNull(entities);
         System.out.println(entities);
-        
+
     }
-    
-    //@Test(expected=HttpClientErrorException.class)
+
+    // @Test(expected=HttpClientErrorException.class)
     public void test4GetMediaEntity() {
-        //Create
+        // Create
         authenticationClient.authenticate();
-        
+
         MediaEntity entity = new MediaEntity();
         entity.setName("Test 123");
         entity.setPath("l1/l2");
-        entity = mediaEntityClient.createMediaEntity(entity, "C:\\Users\\Ranjit\\Pictures\\sample.jpg");
+        entity = mediaEntityClient.createMediaEntity(entity, "C:\\Users\\Ranjit\\Pictures\\happybday.jpg");
         assertNotNull(entity);
-        //Get
+        // Get
         MediaEntity entityRetrieved = mediaEntityClient.getMediaEntity(entity.getAssetId());
         assertNotNull(entityRetrieved);
-        
-        //Delete
+
+        // Delete
         mediaEntityClient.deleteMediaEntity(entity.getAssetId());
         entityRetrieved = mediaEntityClient.getMediaEntity(entity.getAssetId());
-        
+
     }
-    
-    
-    @Test
+
+    // @Test
     public void test5GetMediaEntity() throws Exception {
-        //Create
-        authenticationClient.authenticate();
-        
+        // Create
         MediaEntity entity = new MediaEntity();
         entity.setName("Test 123");
         entity.setPath("l1/l2");
-        entity = mediaEntityClient.createMediaEntity(entity, "C:\\Users\\Ranjit\\Pictures\\sample.jpg");
+        entity = mediaEntityClient.createMediaEntity(entity, "C:\\Users\\Ranjit\\Pictures\\happybday.jpg");
         assertNotNull(entity);
-        //Get
+        // Get
         MediaEntity entityRetrieved = mediaEntityClient.getMediaEntity(entity.getAssetId());
         assertNotNull(entityRetrieved);
-        
-        //Rendition retrieval
+
+        // Rendition retrieval
         String tempLocation = System.getProperty("java.io.tmpdir");
-        
+
         String orig_rend_id = entity.getRevisions().get(0).getRenditions().get(0).getRenditionId();
-//        ResizeCommand command = new ResizeCommand();
-//        command.setHeight(100);
-//        command.setWidth(100);
-//        command.setMaintainAr(true);
-            
+        // ResizeCommand command = new ResizeCommand();
+        // command.setHeight(100);
+        // command.setWidth(100);
+        // command.setMaintainAr(true);
+
         FlopCommand command = new FlopCommand();
-        
-        File f = new File(tempLocation + "\\"+orig_rend_id+".png");
+
+        File f = new File(tempLocation + "\\" + orig_rend_id + ".png");
         FileOutputStream fos = new FileOutputStream(f);
         mediaEntityClient.performAnOperationAndGetImage(orig_rend_id, command, fos);
         fos.close();
-        assertTrue(f.length()>0);
-//        f.delete();
-        
-        //Delete
-//        mediaEntityClient.deleteMediaEntity(entity.getAssetId());
-//        entityRetrieved = mediaEntityClient.getMediaEntity(entity.getAssetId());
-        
+        assertTrue(f.length() > 0);
+        // f.delete();
+
+        // Delete
+        // mediaEntityClient.deleteMediaEntity(entity.getAssetId());
+        // entityRetrieved =
+        // mediaEntityClient.getMediaEntity(entity.getAssetId());
+
+    }
+
+    //@Test
+    public void test6GetMediaEntity() throws Exception {
+        authenticationClient.authenticate();
+        for (int i = 0; i < 10; i++) {
+            // Create
+            MediaEntity entity = new MediaEntity();
+            entity.setName("Test " +i);
+            entity.setPath("l1/l2/l3");
+            entity = mediaEntityClient.createMediaEntity(entity, "C:\\Users\\Ranjit\\Pictures\\happybday.jpg");
+            assertNotNull(entity);
+
+            // Get
+            MediaEntity entityRetrieved = mediaEntityClient.getMediaEntity(entity.getAssetId());
+            assertNotNull(entityRetrieved);
+        }
+
     }
 
 }
